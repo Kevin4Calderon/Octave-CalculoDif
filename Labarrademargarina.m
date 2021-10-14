64105b7e36b1ee1197e36d7c063e98f9f28b9442
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-10-14 14:57:39 -0500

    Update Labarrademargarina.m

--- Labarrademargarina.m
+++ Labarrademargarina.m
@@ -1,1 +1,18 @@
-
+%Octave Scrip
+%Title           :Labarrademargarina
+%Description     :Scrip 
+%Author          :Kevin Yoan Calderón García
+%Date            :14-10-2021
+%Version         :1
+%Usage           :
+%Notes           :Requiere aplicacion octave, usar su linea de comandos
+%                :https://octaveintro.readthedocs.io/en/latest/index.html
+%limpiar variables
+clear
+%Ejemplo minimo de material para envase cilindrico}
+%Rango de 0..1 en i= 0.1
+r=50:0.1:50;
+%Valor de la funcion
+ar =0(r) (pi() * (r.2)) + (2./(r));
+%Funcion a plotear
+y =(pi)) * (r.2)) + (2./(r));
