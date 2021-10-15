36646157c7439a1a6989b334f167e9b65f156c1c
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-10-14 19:41:28 -0500

    Update Labarrademargarina.m

--- Labarrademargarina.m
+++ Labarrademargarina.m
@@ -1,18 +1,22 @@
-%Octave Scrip
-%Title           :Labarrademargarina
-%Description     :Scrip 
-%Author          :Kevin Yoan Calderón García
-%Date            :14-10-2021
-%Version         :1
-%Usage           :
-%Notes           :Requiere aplicacion octave, usar su linea de comandos
-%                :https://octaveintro.readthedocs.io/en/latest/index.html
-%limpiar variables
-clear
-%Ejemplo minimo de material para envase cilindrico}
-%Rango de 0..1 en i= 0.1
-r=50:0.1:50;
-%Valor de la funcion
-ar =0(r) (pi() * (r.2)) + (2./(r));
-%Funcion a plotear
-y =(pi)) * (r.2)) + (2./(r));
+% Título: La barra de margarina
+% Descripción: Script
+% Autor: 
+% Fecha: 20211014
+% Versión: 1
+% De uso: C:\Users\19403219\Documents\Octave
+% Notas: Requiere aplicacion octava, usar su linea de comandos
+%Ejemplo de diemnsiones para gastar menos papel 
+%Rango del valor 
+x=4.7622;
+%Valor de la funcion 
+at=@(x) 2 * x.^2 + 4*x;
+%Funcion a plotear 
+y=108/x.^2 
+%Dibujar x,y
+plot(x,y,'ro-');
+%Titulo
+title(['Material minimo x = ' num2str(x)]);
+%Etiqueta para x
+xlabel(['Min x = ' num2str(x)]);
+%Etiqueta para y
+ylabel(['Min y = ' num2str(x)]);
